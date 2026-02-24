53630dec88268eaa7e0613191b52193ff1007d9d
Nathan OGER <github.sultry568@passmail.net>
2026-02-24 10:20:36 +0000

    Premiers esssais sur dynamo

--- labs/05-projet-capstone-one/src/deploy-project.ts
+++ labs/05-projet-capstone-one/src/deploy-project.ts
@@ -1,3 +1,13 @@
+import {
+  DynamoDBClient,
+  CreateTableCommand,
+  DynamoDBClientConfig,
+  CreateTableInput,
+  PutItemCommand,
+  PutItemInput,
+} from '@aws-sdk/client-dynamodb'; // ES Modules import
+
+
 // Main function to execute all operations
 async function deploy() {
   try {
@@ -6,7 +16,23 @@ async function deploy() {
     // Create S3 and Insert Objects
 
     // Create DynamoDB and Insert Items
+    const config: DynamoDBClientConfig = {};
+    const client = new DynamoDBClient(config);
 
+    const db_name = "ENM-";
+
+    const createTableInput: CreateTableInput = {
+      TableName: db_name,
+      BillingMode: 'PAY_PER_REQUEST',
+      AttributeDefinitions: [
+        { AttributeName: 'Id', AttributeType: 'N' },
+        { AttributeName: 'Name', AttributeType: 'S' },
+        { AttributeName: 'Price', AttributeType: 'N' },
+      ],
+      KeySchema: [
+        { AttributeName: 'Id', KeyType: 'HASH' },
+      ],
+    };
     // Create API Gateway and Configure S3 / DynamoDB Integration
 
     console.log('Project deployed...');
